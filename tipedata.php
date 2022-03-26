7ca779113f103dabd0a1dbecd036cb9eb31186c1
Angelina Balqis Khansa <angelinabalqisk@gmail.com>
2022-03-27 00:29:57 +0700

    first commit

---
+++ tipedata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php 
+			$a ="Hello World!";
+			$b = 99;
+			$c = 9.8;
+			$d = NULL;
+			var_dump ($a,$b,$c,$c);
+		?>
+	</body>
+</html>
+
+	
